fd3beacf3a537c886d43c6bed483c964a8e84bb4
Lansdon Page <lansdon@athena.local>
2013-01-16 11:59:01 -0800

    Initial Commit

---
+++ Repassitory/LRPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LRPAppDelegate.h
+//  Repassitory
+//
+//  Created by Lansdon Page on 1/16/13.
+//  Copyright (c) 2013 Lansdon Page. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Repassitory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Repassitory
+//
+//  Created by Lansdon Page on 1/16/13.
+//  Copyright (c) 2013 Lansdon Page. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LRPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LRPAppDelegate class]));
+    }
+}
